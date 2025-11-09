f6a717302e1f5ce3cef08c9a4a602adcec3baefc
Alexander <pn-alex@mail.ru>
2025-11-09 23:12:20 +0300

    Lesson 1. Akkordeon

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const contents = document.querySelectorAll('.program-line__content')
+
+contents.forEach((elem) => {
+	const title = elem.querySelector('.program-line__title')
+	const descr = elem.querySelector('.program-line__descr')
+
+	title.addEventListener('click', () => {
+		contents.forEach((e) => {
+			e.querySelector('.program-line__descr').classList.remove('active')
+		})
+		descr.classList.toggle('active')
+	})
+})
